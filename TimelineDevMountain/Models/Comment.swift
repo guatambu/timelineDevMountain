303f3d9ab055c31d23b4ed6447a30d08de3418c6
guatambu <guatambu@dundak.com>
2018-07-28 18:57:08 -0700

    completed basic setp of Post data model

---
+++ TimelineDevMountain/Models/Comment.swift
@@ -0,0 +1,36 @@
+//
+//  Comment.swift
+//  TimelineDevMountain
+//
+//  Created by Michael Guatambu Davis on 7/28/18.
+//  Copyright © 2018 DunDak, LLC. All rights reserved.
+//
+
+import Foundation
+
+class Comment {
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ TimelineDevMountain/Models/Post.swift
@@ -0,0 +1,58 @@
+//
+//  Post.swift
+//  TimelineDevMountain
+//
+//  Created by Michael Guatambu Davis on 7/28/18.
+//  Copyright © 2018 DunDak, LLC. All rights reserved.
+//
+
+import UIKit
+
+class Post {
+    
+    // MARK: - Properties
+    
+    let photoData: Data?
+    var timestamp: Date
+    var comments: [Comment]
+    var photo: UIImage {
+        guard   let data = photoData,
+                let foto = UIImage(data: data)
+            else { return #imageLiteral(resourceName: "post_list") }
+        return foto
+    }
+    
+    // MARK: - Initializers
+    
+    // memberwise
+    init(photoData: Data?, timestamp: Date = Date(), comments: [Comment] = []) {
+        self.photoData = photoData
+        self.timestamp = timestamp
+        self.comments = comments
+    }
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
