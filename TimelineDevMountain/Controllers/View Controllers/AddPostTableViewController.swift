d3dc9737376d1885c3abe937f75adaee377479f9
guatambu <guatambu@dundak.com>
2018-07-29 17:25:35 -0700

    set up addPost in storyBoard and wired up to AddPostTVC.swift + selectPhotoButtonTapped()

---
+++ TimelineDevMountain/Controllers/View Controllers/AddPostTableViewController.swift
@@ -0,0 +1,72 @@
+//
+//  AddPostTableViewController.swift
+//  TimelineDevMountain
+//
+//  Created by Michael Guatambu Davis on 7/29/18.
+//  Copyright © 2018 DunDak, LLC. All rights reserved.
+//
+
+import UIKit
+
+class AddPostTableViewController: UITableViewController {
+    
+    // MARK: - Properties
+    
+    @IBOutlet weak var postImageView: UIImageView!
+    @IBOutlet weak var photoCaptionTextField: UITextField!
+    @IBOutlet weak var selectPhotoButtonOutlet: UIButton!
+    
+    
+    // MARK: - ViewController Lifecycle Functions
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+    // MARK: - Actions
+    
+    @IBAction func selectPhotoButtonTapped(_ sender: UIButton) {
+        postImageView.image = #imageLiteral(resourceName: "placeholder_image")
+        selectPhotoButtonOutlet.titleLabel?.text = ""
+    }
+    
+    @IBAction func addPostButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 3
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 1
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
