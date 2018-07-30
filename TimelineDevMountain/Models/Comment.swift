274c98739bffe4e0442cb19e05f7c7339f7462ca
guatambu <guatambu@dundak.com>
2018-07-30 16:25:30 -0700

    completed CloudKit initializers for Comment object model

--- TimelineDevMountain/Models/Comment.swift
+++ TimelineDevMountain/Models/Comment.swift
@@ -7,23 +7,50 @@
 //
 
 import Foundation
+import CloudKit
 
 class Comment {
+    
+    // MARK: - Private Keys
+    
+    fileprivate let commentKey = "comment"
+    fileprivate let textKey = "text"
+    fileprivate let timestampKey = "timestamp"
+    fileprivate let postKey = "post"
+    fileprivate let appleUserReferenceKey = "appleUserReference"
+    
     
     // MARK: - Properties
     
     var text: String
     var timestamp: Date
     var post: Post
+    var ckRecordID: CKRecordID?
+    let appleUserReference: CKReference
     
     
     // MARK: - Initializers
     
     // memberwise
-    init(text: String, timestamp: Date = Date(), post: Post) {
+    init(text: String, timestamp: Date = Date(), post: Post, appleUserReference: CKReference) {
         self.text = text
         self.timestamp = timestamp
         self.post = post
+        self.appleUserReference = appleUserReference
+    }
+    
+    // failable initializer - fetching from cloud
+    init?(ckRecord: CKRecord) {
+        guard   let text = ckRecord[textKey] as? String,
+                let timestamp = ckRecord[timestampKey] as? Date,
+                let post = ckRecord[postKey] as? Post,
+                let appleUserReference = ckRecord[appleUserReferenceKey] as? CKReference
+            else { return nil }
+        
+        self.text = text
+        self.timestamp = timestamp
+        self.post = post
+        self.appleUserReference = appleUserReference
     }
 }
 
@@ -34,6 +61,24 @@ extension Comment: SearchableRecord {
     }
 }
 
+
+extension CKRecord {
+    
+    convenience init(comment: Comment) {
+        
+        // Comment object's existing userID if present, if none, then create one
+        let recordID = comment.ckRecordID ?? CKRecordID(recordName: UUID().uuidString)
+        
+        // designated initializer
+        self.init(recordType: "Comment", recordID: recordID)
+        
+        // set values
+        self.setValue(comment.text, forKey: comment.textKey)
+        self.setValue(comment.timestamp, forKey: comment.timestampKey)
+        self.setValue(comment.post, forKey: comment.postKey)
+        self.setValue(comment.appleUserReference, forKey: comment.appleUserReferenceKey)
+    }
+}
 
 
 
